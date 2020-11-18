789d87b8bf2ee6fec6c8315741dd026ec84fa56f
karthikeyansa <karthikeyansa39@gmail.com>
2020-11-18 22:06:17 +0530

    added files

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path('',views.login,name="login"),
     path('register',views.register,name="register"),
+    path('tasks',views.tasks,name='tasks'),
     path('addtask',views.addTask,name="addTask"),
     path('delete/<int:id>',views.deleteTask,name="deleteTask"),
     path('completed/<int:id>',views.completedTask,name="completedTask"),
--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,72 +1,53 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate,logout as logO,login as logG
+from django.contrib.auth.models import User
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-from .models import Tasks,User
-from .forms import UserForm
-
+from .models import Tasks
 @csrf_exempt
 def login(request):
-    form = UserForm()
     if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(username = username,password = password)
-            if user:
-                request.session['user_id'] = user.id
-                tasks = Tasks.objects.filter(owner = user)
-                return render(request,'mysite/tasks.html',{'tasks': tasks})
-        else:
-            messages.add_message(request, messages.WARNING, 'Invalid Credentials')
-            return redirect(login)
-    return render(request,'mysite/index.html',{'form': form})
-@csrf_exempt
-def register(request):
-    form = UserForm()
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = User.objects.create_user(username = username,password = password)
-            if user:
-                messages.add_message(request, messages.SUCCESS, 'User %s created successfully'%username)
-                return redirect(login)
-        return redirect(register)
-    return render(request,'mysite/index.html',{'form': form})
+        user = authenticate(username = request.POST['username'],password = request.POST['password'])
+        logG(request,user)
+        return redirect(tasks)
+    return render(request,'mysite/index.html')
 
-@csrf_exempt
-def addTask(request):
-    user = User.objects.get(pk = request.session['user_id'])
-    tasks = Tasks.objects.filter(owner = user)
-    if request.method == 'POST':
-        taskname = request.POST['taskname']
-        taskdesc = request.POST['taskdetail']
-        task = Tasks(taskName=taskname,taskDetails=taskdesc,owner=user)
-        task.save()
-        messages.add_message(request, messages.SUCCESS, 'Task %s added successfully'%taskname)
-        return redirect(addTask)
+def tasks(request):
+    tasks = Tasks.objects.filter(owner = request.user)
     return render(request,'mysite/tasks.html',{'tasks':tasks})
 
 @csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        User.objects.create_user(username = request.POST['username'],password = request.POST['password'])
+        messages.add_message(request, messages.SUCCESS, 'User created successfully')
+        return redirect(login)
+    return redirect(register)
+
+@csrf_exempt
+def addTask(request):
+    if request.method == 'POST':
+        Tasks(taskName=request.POST['taskname'],taskDetails=request.POST['taskdetail'],owner=request.user).save()
+        messages.add_message(request, messages.SUCCESS, 'Task added successfully')
+        return redirect(tasks)
+    return redirect(tasks)
+
+@csrf_exempt
 def deleteTask(request,id):
-    task = Tasks.objects.get(pk = id)
-    messages.add_message(request, messages.SUCCESS, 'Task %s Deleted successfully'%task.taskName)
-    task.delete()
-    return redirect(addTask)
+    Tasks.objects.get(pk = id).delete()
+    messages.add_message(request, messages.SUCCESS, 'Task Deleted successfully')
+    return redirect(tasks)
 
 def completedTask(request,id):
     task = Tasks.objects.get(pk = id)
     task.completed = True
     task.save()
     messages.add_message(request, messages.SUCCESS, 'Task %s Completed successfully'%task.taskName)
-    return redirect(addTask)
+    return redirect(tasks)
 
 def logout(request):
-    del request.session['user_id']
+    logO(request)
     messages.add_message(request, messages.SUCCESS, 'Logged Out')
     return redirect(login)
